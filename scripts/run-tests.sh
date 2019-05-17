570b57aed72b73b7cb926202774eadd22a791009
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-05-17 21:51:20 +0200

    travis update

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 echo "Checking docker-multiple-wordpress_wp-1"
-curl -I localhost:$(docker ps|grep docker-multiple-wordpress_wp1 |sed 's/.*0.0.0.0://g'|sed 's/->.*//g')/wp-admin/install.php
+export wp1=$(docker ps|grep docker-multiple-wordpress_wp1 |sed 's/.*0.0.0.0://g'|sed 's/->.*//g')
+curl -I localhost:${wp1}/wp-admin/install.php
 echo "Checking docker-multiple-wordpress_wp-2"
-curl -I localhost:$(docker ps|grep docker-multiple-wordpress_wp2 |sed 's/.*0.0.0.0://g'|sed 's/->.*//g')/wp-admin/install.php
+export wp1=$(docker ps|grep docker-multiple-wordpress_wp2 |sed 's/.*0.0.0.0://g'|sed 's/->.*//g')
+curl -I localhost:${wp2}/wp-admin/install.php
 echo "Checking docker-multiple-wordpress_wp-3"
-curl -I localhost:$(docker ps|grep docker-multiple-wordpress_wp3 |sed 's/.*0.0.0.0://g'|sed 's/->.*//g')/wp-admin/install.php
+export wp1=$(docker ps|grep docker-multiple-wordpress_wp3 |sed 's/.*0.0.0.0://g'|sed 's/->.*//g')
+curl -I localhost:${wp3}/wp-admin/install.php
 
 echo "Checking docker-multiple-wordpress_haproxy"
 curl -I localhost:8080
